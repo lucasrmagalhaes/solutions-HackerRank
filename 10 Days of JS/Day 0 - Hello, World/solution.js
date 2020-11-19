60f5166e3f057495b342bcbb803673b8f3a9e407
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2020-11-19 18:55:07 -0300

    output

--- 10 Days of JS/Day 0 - Hello, World/solution.js
+++ 10 Days of JS/Day 0 - Hello, World/solution.js
@@ -2,3 +2,9 @@ function greeting(parameterVariable) {
     console.log('Hello, World!');
     console.log(parameterVariable)    
 }
+
+/* 
+ * Expected Output:
+ * Hello, World!
+ * Welcome to 10 Days of JavaScript!
+ */
