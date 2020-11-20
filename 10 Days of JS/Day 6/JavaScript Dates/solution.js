0989ccd6840716f9ac99fc3829cbc1ce9379c9cb
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2020-11-20 17:51:46 -0300

    day 6

---
+++ 10 Days of JS/Day 6/JavaScript Dates/solution.js
@@ -0,0 +1,17 @@
+// The days of the week are: "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"
+function getDayName(dateString) {
+    let dayName;
+    
+    var day_names = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+    var date = new Date(dateString);
+    
+    dayName = day_names[date.getDay()];
+    
+    return dayName;
+}
+
+/**
+ * Expected Output:
+ * Sunday
+ * Wednesday
+ */
